886bbdad98826bff61e386dac6bfe86dda517c04
Danila Lazarew <koman4bk@bk.ru>
2021-01-30 13:55:39 +0300

    изменения в html, css, js. В JS применено ООП

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,20 +1,36 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (product, img='http://placehold.it/200x150') => {
-    return `<div class="product-item">
-                <h3>${product.title}</h3>
-                <img src="${img}">
-                <p>${product.price}</p>
+class ProductList{
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000},
+            {id: 2, title: 'Mouse', price: 20},
+            {id: 3, title: 'Keyboard', price: 200},
+            {id: 4, title: 'Gamepad', price: 50},
+        ];
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        for( let product of this.goods){
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend',productObj.render());
+        }
+    }
+}
+class ProductItem{
+    constructor(product, img = 'https://placehold.it/200x150'){
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
                 <button class="buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
-};
-
-renderPage(products);
+                </div>`
+    }
+}
+let list = new ProductList();
+list.render();
