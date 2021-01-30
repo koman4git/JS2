e688bae5ef261df703fe08994f8db68bdba5f880
Danila Lazarew <koman4bk@bk.ru>
2021-01-30 18:57:40 +0300

    добавлены картинки для каждого товара

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -2,10 +2,10 @@ class ProductList{
     constructor(container = '.products'){
         this.container = container;
         this.goods = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 20},
-            {id: 3, title: 'Keyboard', price: 200},
-            {id: 4, title: 'Gamepad', price: 50},
+            {id: 1, title: 'Notebook', price: 2000, img: 'https://img.mvideo.ru/Pdb/30043694b.jpg'},
+            {id: 2, title: 'Mouse', price: 20, img: 'https://img.mvideo.ru/Pdb/50038582b.jpg'},
+            {id: 3, title: 'Keyboard', price: 200, img: 'https://img.mvideo.ru/Pdb/50141063b.jpg'},
+            {id: 4, title: 'Gamepad', price: 50, img: 'https://img.mvideo.ru/Pdb/50038762b.jpg'},
         ];
     }
     render(){
@@ -16,12 +16,13 @@ class ProductList{
         }
     }
 }
+
 class ProductItem{
-    constructor(product, img = 'https://placehold.it/200x150'){
+    constructor(product){
         this.title = product.title;
         this.price = product.price;
         this.id = product.id;
-        this.img = img;
+        this.img = product.img;
     }
     render(){
         return `<div class="product-item" data-id="${this.id}">
@@ -32,5 +33,6 @@ class ProductItem{
                 </div>`
     }
 }
+
 let list = new ProductList();
 list.render();
