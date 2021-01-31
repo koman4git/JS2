8eb3033eb0983b624f1707e6fb0b7e3212474590
Danila Lazarew <koman4bk@bk.ru>
2021-01-31 12:51:30 +0300

    добавлены изменения согласно ДЗ № 2

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -7,13 +7,21 @@ class ProductList{
             {id: 3, title: 'Keyboard', price: 200, img: 'https://img.mvideo.ru/Pdb/50141063b.jpg'},
             {id: 4, title: 'Gamepad', price: 50, img: 'https://img.mvideo.ru/Pdb/50038762b.jpg'},
         ];
+        this.allProducts = [];
     }
     render(){
         const block = document.querySelector(this.container);
         for( let product of this.goods){
             const productObj = new ProductItem(product);
-            block.insertAdjacentHTML('beforeend',productObj.render());
+            this.allProducts.push(productObj);            block.insertAdjacentHTML('beforeend',productObj.render());
         }
+    }
+    countSum(){
+        let sum = 0;
+        for(let product of this.goods){
+            sum += product.price;
+        }
+        alert('Сумма представленных товаров составляет: ' + sum + ' условных единиц');
     }
 }
 
@@ -34,5 +42,26 @@ class ProductItem{
     }
 }
 
+class Basket {
+    addGoods(){
+        
+    }
+    cleanBasket(){
+        
+    }
+    render(){
+        
+    }
+}
+class itemBasket {
+    deleteGoods(){
+        
+    }
+    render(){
+        
+    }
+}
 let list = new ProductList();
 list.render();
+list.countSum();
+    
